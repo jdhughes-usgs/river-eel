8d5637895702eab4f0ed722d090f32e0bf964188
Joseph D Hughes <jdhughes@usgs.gov>
2017-08-16 10:02:46 -0400

    Add a few additional tests

---
+++ examples/mf6/test103_drn_mvr/mf2005/runmf2005.bat
@@ -0,0 +1,4 @@
+@echo off
+echo Running MF2005...
+..\..\..\..\mf2005\msvs\Release\mf2005.exe testdrnmvr.nam
+pause
---
+++ examples/mf6/test103_drn_mvr/mf2005/runmf2005d.bat
@@ -0,0 +1,4 @@
+@echo off
+echo Running MF2005...
+..\..\..\..\mf2005\msvs\Debug\mf2005.exe testdrnmvr.nam
+pause
