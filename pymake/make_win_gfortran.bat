e2323669fff2016dc240e0b2ff4a4536cd3a568d
Joseph D Hughes <jdhughes@usgs.gov>
2017-08-16 15:33:48 -0400

    Move pakdata_pr code into uzf_ar to test gfortran7 failure

--- pymake/make_win_gfortran.bat
+++
@@ -1,5 +0,0 @@
-rem set PATH=%PATH%;Z:\Users\langevin\langevin\temp\gcc-5-win64\bin
-set PATH=%PATH%;Z:\Users\langevin\langevin\software\gfortran\mingw64\bin
-gfortran --version
-python makebin.py -fc gfortran -sd -mc ../src mf6.exe
-pause
--- pymake/make_win_intel.bat
+++
@@ -1,4 +0,0 @@
-python makebin.py -fc ifort -sd -mc -ar intel64 ../src mf6.exe
-rem python makebin.py -fc ifort -sd -mc -ar ia32 ../src mf6_ia32.exe
-rem python makebin.py -fc gfortran -sd -mc -dr -mf ../src mf6
-pause
